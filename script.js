c91de5dbfcd0a696d79f82e2240d30215c3bd655
Alif-Hossen <naimuralif1.0@gmail.com>
2025-08-28 14:49:20 -0600

    After First Functionality Add

--- script.js
+++ script.js
@@ -1,12 +1,140 @@
-// console.log("is It Working?")
+// // console.log("is It Working?")
 
 
 
-document.getElementById("heart")
-    .addEventListener("click", function(){
-        // console.log("Heart Clicked");
-        const  heartValue = document.getElementById("heart_main").value 
-        console.log(heartValue);
+//  document.getElementById("heart1")
+//     .addEventListener("click", function(){
+//         console.log("Heart Clicked");
 
-        
-    })
+//         const heartValue = parseInt (document.getElementById("heart_main").innerText)
+//         console.log(heartValue) 
+
+//         const totalHeart  = heartValue + 1
+//         console.log(totalHeart)
+
+//         document.getElementById("heart_main").innerText = totalHeart
+
+//     })
+
+//  document.getElementById("heart2")
+//     .addEventListener("click", function(){
+//         console.log("Heart Clicked");
+
+//         const heartValue = parseInt (document.getElementById("heart_main").innerText)
+//         console.log(heartValue) 
+
+//         const totalHeart  = heartValue + 1
+//         console.log(totalHeart)
+
+//         document.getElementById("heart_main").innerText = totalHeart
+
+//     })
+
+//  document.getElementById("heart3")
+//     .addEventListener("click", function(){
+//         console.log("Heart Clicked");
+
+//         const heartValue = parseInt (document.getElementById("heart_main").innerText)
+//         console.log(heartValue) 
+
+//         const totalHeart  = heartValue + 1
+//         console.log(totalHeart)
+
+//         document.getElementById("heart_main").innerText = totalHeart
+//     })
+    
+//  document.getElementById("heart4")
+//     .addEventListener("click", function(){
+//         console.log("Heart Clicked");
+
+//         const heartValue = parseInt (document.getElementById("heart_main").innerText)
+//         console.log(heartValue) 
+
+//         const totalHeart  = heartValue + 1
+//         console.log(totalHeart)
+
+//         document.getElementById("heart_main").innerText = totalHeart
+//     })
+
+//  document.getElementById("heart5")
+//     .addEventListener("click", function(){
+//         console.log("Heart Clicked");
+
+//         const heartValue = parseInt (document.getElementById("heart_main").innerText)
+//         console.log(heartValue) 
+
+//         const totalHeart  = heartValue + 1
+//         console.log(totalHeart)
+
+//         document.getElementById("heart_main").innerText = totalHeart
+//     })
+
+//  document.getElementById("heart6")
+//     .addEventListener("click", function(){
+//         console.log("Heart Clicked");
+
+//         const heartValue = parseInt (document.getElementById("heart_main").innerText)
+//         console.log(heartValue) 
+
+//         const totalHeart  = heartValue + 1
+//         console.log(totalHeart)
+
+//         document.getElementById("heart_main").innerText = totalHeart
+//     })
+//  document.getElementById("heart7")
+//     .addEventListener("click", function(){
+//         console.log("Heart Clicked");
+
+//         const heartValue = parseInt (document.getElementById("heart_main").innerText)
+//         console.log(heartValue) 
+
+//         const totalHeart  = heartValue + 1
+//         console.log(totalHeart)
+
+//         document.getElementById("heart_main").innerText = totalHeart
+//     })
+//  document.getElementById("heart8")
+//     .addEventListener("click", function(){
+//         console.log("Heart Clicked");
+
+//         const heartValue = parseInt (document.getElementById("heart_main").innerText)
+//         console.log(heartValue) 
+
+//         const totalHeart  = heartValue + 1
+//         console.log(totalHeart)
+
+//         document.getElementById("heart_main").innerText = totalHeart
+//     })
+//  document.getElementById("heart9")
+//     .addEventListener("click", function(){
+//         console.log("Heart Clicked");
+
+//         const heartValue = parseInt (document.getElementById("heart_main").innerText)
+//         console.log(heartValue) 
+
+//         const totalHeart  = heartValue + 1
+//         console.log(totalHeart)
+
+//         document.getElementById("heart_main").innerText = totalHeart
+//     })
+
+
+function handleHeartClick() {
+    console.log("Heart Clicked");
+
+    const heartElement = document.getElementById("heart_main");
+    let heartValue = parseInt(heartElement.innerText);
+    console.log("Current Heart Value:", heartValue);
+
+    heartValue += 1;
+    console.log("Updated Heart Value:", heartValue);
+
+    heartElement.innerText = heartValue;
+}
+
+for (let i = 1; i <= 9; i++) {
+    const heart = document.getElementById("heart" + i);
+    if (heart) {
+        heart.addEventListener("click", handleHeartClick);
+    }
+}
