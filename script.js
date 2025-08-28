4d44d92af7672b3a83a18b0a27a58c8d089a0fc3
Alif-Hossen <naimuralif1.0@gmail.com>
2025-08-28 14:23:24 -0600

    After UI design

---
+++ script.js
@@ -0,0 +1,12 @@
+// console.log("is It Working?")
+
+
+
+document.getElementById("heart")
+    .addEventListener("click", function(){
+        // console.log("Heart Clicked");
+        const  heartValue = document.getElementById("heart_main").value 
+        console.log(heartValue);
+
+        
+    })
